bbb0c16ced41dc81a68fd97194f6f868f3a61085
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-11-27 00:25:29 +0300

    created model Doctor .gitignore

---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from config import settings
+from users.models import NULLABLE
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    surname = models.CharField(max_length=100, verbose_name="Отчество", **NULLABLE)
+    specialization = models.CharField(max_length=100, verbose_name="Специализация")
+    experience = models.IntegerField(verbose_name="Стаж работы")
+    education = models.TextField(verbose_name="Образование")
+
+    class Meta:
+        verbose_name = "Доктор"
+        verbose_name_plural = "Доктора"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.surname} ({self.specialization})"
